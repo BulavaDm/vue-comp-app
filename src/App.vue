cf6013b922074f95130c69afaf067b76ec410e5d
BulavaDm <bulava.dmitrij@gmail.com>
2023-10-25 18:35:31 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="increment">
+        {{ refState }}
+    </button>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    const refState = ref(0)
+
+    function increment() {
+        refState.value++
+    }
+</script>
+
+
+
